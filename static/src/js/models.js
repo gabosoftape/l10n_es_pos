3ffcaf57a171eb98b0cd5a164fec75d049662c03
gabriel pabon <gabosoft.ape@gmail.com>
2018-12-20 11:26:21 -0500

    update log

--- static/src/js/models.js
+++ static/src/js/models.js
@@ -29,7 +29,7 @@ odoo.define('l10n_es_pos.models', function (require) {
             if (this.pushed_simple_invoices.indexOf(this.config.l10n_es_simplified_invoice_number) > -1) {
               console.log("aqui se obtuvo (get simple inv next number).."+this.config.l10l10n_es_simplified_invoice_number);
                 ++this.config.l10n_es_simplified_invoice_number;
-                console.log("aqui se aumento ? "+this.config.l10n_es_simplified_invoice_number_normal);
+                console.log("aqui se aumento ? "+this.config.l10n_es_simplified_invoice_number);
             }
             console.log("no entramos al if");
             return this.config.l10n_es_simplified_invoice_prefix+this.get_padding_simple_inv(this.config.l10n_es_simplified_invoice_number);
