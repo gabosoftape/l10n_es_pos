58debd2ece0afe93411f5eeac23ec61be7019d14
gabriel pabon <gabosoft.ape@gmail.com>
2018-12-20 13:45:28 -0500

    update validation pay method

--- static/src/js/screens.js
+++ static/src/js/screens.js
@@ -17,7 +17,35 @@ odoo.define('l10n_es_pos.screens', function (require) {
             var lines = this.pos.get_order().get_paymentlines();
                 if (lines.length<=1) {
                   console.log("solo hay un medio de pago");
-                if (lines[0].name.includes("REDEBAN")) {
+                if (lines[0].name =="REDEBAN BCM (COP)") {
+                    if (this.pos.config.iface_l10n_es_simplified_invoice) {
+                      console.log('iface_l10n_es_simplified_invoice esta en True por eso haremos lo siguiente ');
+                        var order = this.pos.get_order();
+                        if (below_limit) {
+                            order.set_simple_inv_number();
+                            var dias = order.date_order;
+                            console.log("el primer cupon seria para .. "+dias);
+                            console.log("seteamos el numero de factura");
+                        } else {
+                            // Force invoice above limit. Online is needed.
+                            order.to_invoice = true;
+                        }
+                      }
+                } else if (lines[0].name =="REDEBAN BPI (COP)")) {
+                    if (this.pos.config.iface_l10n_es_simplified_invoice) {
+                      console.log('iface_l10n_es_simplified_invoice esta en True por eso haremos lo siguiente ');
+                        var order = this.pos.get_order();
+                        if (below_limit) {
+                            order.set_simple_inv_number();
+                            var dias = order.date_order;
+                            console.log("el primer cupon seria para .. "+dias);
+                            console.log("seteamos el numero de factura");
+                        } else {
+                            // Force invoice above limit. Online is needed.
+                            order.to_invoice = true;
+                        }
+                      }
+                }else if (lines[0].name ==("REDEBAN VENECIA (COP)")) {
                     if (this.pos.config.iface_l10n_es_simplified_invoice) {
                       console.log('iface_l10n_es_simplified_invoice esta en True por eso haremos lo siguiente ');
                         var order = this.pos.get_order();
